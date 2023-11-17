ef4fb62695cab1839697ca507a1903b28adcb22c
Matthew Hess <matthew9510@gmail.com>
2023-11-17 01:18:48 -0800

    feat: Updated App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Wrapper } from './App.styles';
+import IntuitNav from './Components/IntuitNav/IntuitNav';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper className='App'>
+      <IntuitNav />
+      <Footer />
+    </Wrapper>
   );
 }
 
